a2650cf95020a732e38009b84b24a0d565f83fae
srkramesh888 <68192853+srkramesh888@users.noreply.github.com>
2020-12-19 19:35:43 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class ram(models.Model):
+    Name=models.CharField(max_length=200)
+    Password=models.CharField(max_length=200)
+    Email_ID=models.EmailField()
+    Gender=models.CharField(max_length=200)
+    Date_of_Birth=models.CharField(max_length=200)
+    Address=models.CharField(max_length=200)
+    Phone_Number=models.CharField(max_length=200)
+    Photo=models.ImageField(null=True,blank=True)
+class Meta:
+    db_table="try_ram"
+
+
+
+
+
