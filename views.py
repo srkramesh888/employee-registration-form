60e6c528b4945d5a045354aeef67c2df53aae71d
srkramesh888 <68192853+srkramesh888@users.noreply.github.com>
2020-12-19 19:33:14 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from . import views
+from .models import ram
+from .forms import frm
+    
+
+# Create your views here.
+def index(request):
+    return render(request,"register.html")
+def createpst(request):
+    
+    name = request.POST.get("Name")
+    password= request.POST.get("Password")
+    email= request.POST.get("Email_ID")
+    gen= request.POST.get("Gender")
+    dob= request.POST.get("Date_of_Birth")
+    address= request.POST.get("Address")
+    phone= request.POST.get("Phone_Number")
+    photo=request.POST.get("Photo")
+    a=ram(Name=name,Password=password,Email_ID= email,Gender=gen,Date_of_Birth=dob,Address=address,Phone_Number=phone,Photo=photo)
+    a.save()
+    return render(request,"login.html")
