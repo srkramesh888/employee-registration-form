06e4cb8faabe3d1f5d2bb97334854fde89b35664
srkramesh888 <68192853+srkramesh888@users.noreply.github.com>
2020-12-20 10:09:18 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import ram
+from .models import Ram
 class frm(forms.ModelForm):
     class Meta:
-        model = ram
+        model = Ram
      
         fields = "__all__"
